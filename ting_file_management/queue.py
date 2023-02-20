03bab8699fdb5f92935c309a5cdc0c193776bd72
WalterTNFC <walter.tad@hotmail.com>
2023-02-20 15:37:37 -0300

    feat: Verify req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,19 +1,25 @@
 from ting_file_management.abstract_queue import AbstractQueue
-# teste
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        # Inicializando a lista
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        # Uso do len para determinação do tamanho da lista
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        # Append para adicionar value ao final
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # pop para remover o ultimo item mas nao exluir
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        queue = self.queue
+        if index >= 0 and index <= (len(queue) - 1):
+            return queue[index]
+        raise IndexError
