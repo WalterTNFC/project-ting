6b3517873525745483604a6a54fc8513a9d94934
WalterTNFC <walter.tad@hotmail.com>
2023-02-22 11:07:24 -0300

    feat: Fix lint

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,9 +2,10 @@ import sys
 from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
 
-def process(path_file, instance):
+
+def process(path_file, instance: Queue):
     # Acessar a lista do texto importado:
-    list_to_convert = txt_importer(path_file);
+    list_to_convert = txt_importer(path_file)
 
     # Cria dicionário da lista para ser salvo
     new_data = {
@@ -15,11 +16,12 @@ def process(path_file, instance):
 
     # Se já existir, não é salvo
     for fileListed in instance.queue:
-        if  fileListed['nome_do_arquivo'] == new_data['nome_do_arquivo']:
+        if fileListed['nome_do_arquivo'] == new_data['nome_do_arquivo']:
             return None
 
     instance.enqueue(new_data);
-    print(new_data, file=sys.stdout);
+    print(new_data, file=sys.stdout)
+
 
 def remove(instance):
     """Aqui irá sua implementação"""
